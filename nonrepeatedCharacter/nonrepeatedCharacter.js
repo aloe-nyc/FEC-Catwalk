df24b6a379eef641cd5c07d8087a5b98417a156e
swizzlestix09 <angely.rodriguez@gmail.com>
2021-06-22 10:00:04 -0400

    thoughts not completed

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,22 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+
 };
+
+firstNonRepeatedCharacter('AACBDB');
+
+//console.log(firstNonRepeatedCharacter('ABA')) //'B'
+//console.log(firstNonRepeatedCharacter('')) //''
+// console.log(firstNonRepeatedCharacter('1244634r')) //'r' (should ignore numbers?)
+// console.log(firstNonRepeatedCharacter('aBbAaZ'))//'Z'
+// console.log(firstNonRepeatedCharacter('aaaAAaa')) // ''
+
+//previously used an object to do this problem
+//would like to try figuring out a different solution
+//maybe iteration
+//sliceing 1 letter from index
+//if char is included in modified string
+//keep going
+//if not return character from original string
+//i like this idea -- not working though
