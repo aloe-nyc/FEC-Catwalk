8ab36977c515d85d7f77a2250573af95b7807284
swizzlestix09 <angely.rodriguez@gmail.com>
2021-06-22 10:39:01 -0400

    completed but messy

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,16 +7,26 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
+  //turn string into an array
+  var stringCopy = string.split('');
+  var stringCopyCopy = string.split('');
+  var test = 1;
+  //make a copy
+  for ( var i = 0; i < stringCopy.length; i++ ) {
+    var char = stringCopy[i]
+    var removed = stringCopyCopy.splice(i, 1);
+    if ( stringCopyCopy.includes(char) === false) {
+      return string[i]
+    }
+    stringCopyCopy = string.split('');
+  }
 
 };
 
-firstNonRepeatedCharacter('AACBDB');
+console.log( firstNonRepeatedCharacter('AACBDB') ); //C
+console.log(firstNonRepeatedCharacter('ABA')) //'B'
+console.log(firstNonRepeatedCharacter('')) //''
 
-//console.log(firstNonRepeatedCharacter('ABA')) //'B'
-//console.log(firstNonRepeatedCharacter('')) //''
-// console.log(firstNonRepeatedCharacter('1244634r')) //'r' (should ignore numbers?)
-// console.log(firstNonRepeatedCharacter('aBbAaZ'))//'Z'
-// console.log(firstNonRepeatedCharacter('aaaAAaa')) // ''
 
 //previously used an object to do this problem
 //would like to try figuring out a different solution
@@ -26,3 +36,5 @@ firstNonRepeatedCharacter('AACBDB');
 //keep going
 //if not return character from original string
 //i like this idea -- not working though
+
+//I got it to work but it does NOT handle lower case - gonna have to continue with it when I have time
