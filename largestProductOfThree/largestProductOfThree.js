7b2732a2f1c18cd80b3f6064a303811cacf376a0
swizzlestix09 <angely.rodriguez@gmail.com>
2021-06-29 09:51:28 -0400

    Solved w/ EC

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -8,6 +8,37 @@
  */
 
 
-var largestProductOfThree = function(array) {
-  // TODO: everything
+let largestProductOfThree = (array) => {
+  let product = 0;
+  let index = 0
+  while (index < array.length - 2) {
+    for ( let i = 1; i < array.length; i++ ) {
+      if (typeof array[i]!== 'number') return null;
+      if (array[i+1] === undefined) continue;
+
+      let total = (array[index]) * (array[i]) * (array[i+1]);
+
+      if ( Math.abs(total) > Math.abs(product) ) {
+        product = total
+      }
+    }
+    index++;
+  }
+  return product;
 };
+
+console.log(largestProductOfThree([2, 1, 3, 7]) === 42)
+console.log(largestProductOfThree([2, 1, 2, 2, 2, 3, 7]) === 42)
+console.log(largestProductOfThree([2, 1, 3, -7]) === -42);
+console.log(largestProductOfThree([2, 1, 'a', 7]))
+
+// while (secondDig < array.length - 2) {
+//   console.log(num, array[firstDig], array[secondDig])
+//   tempProd = num * array[firstDig] * array[secondDig];
+//   if (Math.abs(tempProd) > product){
+//   product = tempProd;
+//   }
+//   firstDig++;
+//   secondDig++;
+//   tempProd = 0;
+// }
